afc984dbd5cd51c1a7b350a1b1f207c2f3c5042a
KhuramKTS <khuram660@yahoo.com>
2023-01-26 11:59:23 +0500

    convert to any media format

--- MediaConvertor.py
+++ MediaConvertor.py
@@ -1,8 +1,10 @@
 print("Media Convertor - Build By @KhuramKTS")
 import subprocess
 
-input_file = input("Enter the name of a video file with path: ")
-output_file = input_file.split(".")[0] + ".mp3"
+input_file = input("Enter the name of a media file with path: ")
+
+output_format = input("Enter the desired format for the output file (e.g. mp3, wav, etc.): ")
+output_file = input_file.split(".")[0] + "." + output_format
 
 subprocess.run(["ffmpeg", "-i", input_file, output_file])
 print("File converted successfully to: " + output_file)
