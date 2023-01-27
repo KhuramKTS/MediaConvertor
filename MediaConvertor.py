6ac7df28eb424bb1330b2a63854f729b848ad11b
KhuramKTS <khuram660@yahoo.com>
2023-01-27 18:38:39 +0500

    Error reporting

--- MediaConvertor.py
+++ MediaConvertor.py
@@ -1,13 +1,16 @@
 print("Media Convertor - Build By @KhuramKTS")
 import subprocess
+try:
+    input_file = input("Enter the name of a media file with path: ")
 
-input_file = input("Enter the name of a media file with path: ")
+    output_format = input("Enter the desired format for the output file (e.g. mp3, wav, etc.): ")
+    output_file = input_file.split(".")[0] + "." + output_format
 
-output_format = input("Enter the desired format for the output file (e.g. mp3, wav, etc.): ")
-output_file = input_file.split(".")[0] + "." + output_format
+    subprocess.run(["ffmpeg", "-i", input_file, output_file])
+    print("File converted successfully to: " + output_file)
 
-subprocess.run(["ffmpeg", "-i", input_file, output_file])
-print("File converted successfully to: " + output_file)
+except Exception as e:
+    print(e)
 
 print("Media Convertor - Build By @KhuramKTS")
 input("Press any key to exit...")
